3ce6cfd94db334efc9f458bddaf60aa71ed1ee7a
duy <nmnduy@gmail.com>
2025-04-08 16:53:45 +0700

    init

---
+++ cmd/alert/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+)
+
+func main() {
+	fmt.Println("Alert service starting...")
+
+	// Get recipient email from environment variable
+	recipientEmail := os.Getenv("ALERT_EMAIL")
+	if recipientEmail == "" {
+		fmt.Println("Error: ALERT_EMAIL environment variable is not set")
+		os.Exit(1)
+	}
+
+	// Parse command line arguments
+	messagePtr := flag.String("message", "", "The message to send via email")
+	flag.Parse()
+
+	if *messagePtr == "" {
+		fmt.Println("Error: -message flag is required")
+		os.Exit(1)
+	}
+
+	// Create new AWS session
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	// Create SES client
+	svc := ses.New(sess)
+
+	// Send email
+	_, err := svc.SendEmail(&ses.SendEmailInput{
+		Destination: &ses.Destination{
+			ToAddresses: []*string{aws.String(recipientEmail)},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Text: &ses.Content{
+					Data: aws.String(*messagePtr),
+				},
+			},
+			Subject: &ses.Content{
+				Data: aws.String("Alert Notification"),
+			},
+		},
+		Source: aws.String(recipientEmail), // Replace with a verified SES email if different
+	})
+
+	if err != nil {
+		fmt.Println("Error sending email:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Email sent successfully to:", recipientEmail)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module gitlab.com/nmnduy/alert
+
+go 1.23.4
+
+require (
+	github.com/aws/aws-sdk-go v1.55.6 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
